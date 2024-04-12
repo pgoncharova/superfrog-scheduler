501e493f54bc5c112f7dc2513dd4bd96e50cb216
pgoncharova <113141984+pgoncharova@users.noreply.github.com>
2024-04-12 10:19:43 -0500

    Delete Customer

--- src/main/java/edu/tcu/cs/superfrogscheduler/customer/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.tcu.cs.superfrogscheduler.customer;
-
-import edu.tcu.cs.superfrogscheduler.customer.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-}
--- src/test/java/edu/tcu/cs/superfrogscheduler/customer/CustomerServiceTest.java
+++
@@ -1,7 +0,0 @@
-package edu.tcu.cs.superfrogscheduler.customer;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class CustomerServiceTest {
-
-}
