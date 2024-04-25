1ed520c054704557215d875f4933f5fa27faf95f
DUY PHAM <d.pham1801@tcu.edu>
2024-04-25 11:37:28 -0500

    add a login page

---
+++ frontend/src/components/Login/Login.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="container">
+            <label for="username">Username: </label>
+            <input class="username">
+            <label for="password">Password: </label>
+            <input class="password">
+            <button>Login</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-content: center;
+    }
+</style>
