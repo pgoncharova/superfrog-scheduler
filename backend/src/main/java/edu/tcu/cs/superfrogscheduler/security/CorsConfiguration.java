851e1ddbd706bd3c9b042e06882cfdba19df4114
DUY PHAM <d.pham1801@tcu.edu>
2024-05-01 10:20:34 -0500

    update url

--- backend/src/main/java/edu/tcu/cs/superfrogscheduler/security/CorsConfiguration.java
+++ backend/src/main/java/edu/tcu/cs/superfrogscheduler/security/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://superfrogscheduler-c56w7gj6n-dpham123occs-projects.vercel.app/", "https://superfrog-container-frontend.nicerock-3516d100.eastus.azurecontainerapps.io/") // Allow this origin to make requests
+                        .allowedOrigins("http://localhost:5173", "https://superfrogscheduler-69tmp0f7b-dpham123occs-projects.vercel.app", "https://superfrog-container-frontend.nicerock-3516d100.eastus.azurecontainerapps.io") // Allow this origin to make requests
                         .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
                         .allowedHeaders("*") // Allow all headers
                         .allowCredentials(true); // Include cookies in the requests; // Enable CORS for the whole application.
